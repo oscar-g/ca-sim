43f62cf7bc8e685867cbf27f53f6b91b0f0ae3f2
Oscar Gonzalez <oscarg610@gmail.com>
2018-09-01 18:39:07 -0400

    update spec

--- test/simulation/LifeSimulator.spec.ts
+++ test/simulation/LifeSimulator.spec.ts
@@ -21,26 +21,25 @@ describe('LifeSimulator', () => {
         maxTurns: 1,
         neighborhoodSize: 3,
       };
-
       specify('Any live cell with fewer than two live neighbors dies, as if by under population.', () => {
         const sim = new LifeSimulator(config, [
-            [0, 0, 0],
-            [0, 1, 0],
-            [0, 0, 1],
+            [0, 1, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 1, 1],
+            [0, 0, 0, 0],
         ]);
         const expectDeadLocations: Location[] = [
-          { x: 1, y: 1 },
-          { x: 3, y: 3 },
+          { x: 1, y: 0 },
+          { x: 2, y: 2 },
         ];
 
         sim.run();
 
         expectDeadLocations.forEach(({ x, y }) => {
-          const data = sim.state.data.get({ x, y });
-          expect(data.length).eq(0, `Unexpected living cell at x=${x},y=${y}`);
+          const data = sim.state.data.get({ x, y, state: 0 });
+          expect(data.length).eq(0, `Expected dead cell at x=${x},y=${y}`);
         });
       });
-
       specify('Any live cell with two or three live neighbors lives on to the next generation.', () => {
         const sim = new LifeSimulator(config, [
           [0, 0, 0, 0],
@@ -57,11 +56,10 @@ describe('LifeSimulator', () => {
         sim.run();
 
         expectLivingLocations.forEach(({ x, y }) => {
-          const data = sim.state.data.get({ x, y });
-          expect(data.length).eq(1, `Unexpected dead cell at x=${x},y=${y}`);
+          const data = sim.state.data.get({ x, y, state: 1 });
+          expect(data.length).eq(1, `Expected live cell at x=${x},y=${y}`);
         });
       });
-
       specify('Any live cell with more than three live neighbors dies, as if by overpopulation.', () => {
         const sim = new LifeSimulator(config, [
           [0, 0, 0, 0],
@@ -77,8 +75,8 @@ describe('LifeSimulator', () => {
         sim.run();
 
         expectDeadLocations.forEach(({ x, y }) => {
-          const data = sim.state.data.get({ x, y });
-          expect(data.length).eq(0, `Unexpected living cell at x=${x},y=${y}`);
+          const data = sim.state.data.get({ x, y, state: 0 });
+          expect(data.length).eq(0, `Expected dead cell at x=${x},y=${y}`);
         });
       });
       specify('Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.', () => {
@@ -91,7 +89,7 @@ describe('LifeSimulator', () => {
 
         const expectLivingLocations: Location[] = [
           { x: 1, y: 2 },
-          { x: 3, y:2 },
+          { x: 3, y: 2 },
         ];
 
         const expectDeadLocations: Location[] = [
@@ -104,13 +102,13 @@ describe('LifeSimulator', () => {
         sim.run();
 
         expectLivingLocations.forEach(({ x, y }) => {
-          const data = sim.state.data.get({ x, y });
-          expect(data.length).eq(1, `Unexpected dead cell at x=${x},y=${y}`);
+          const data = sim.state.data.get({ x, y, state: 1 });
+          expect(data.length).eq(1, `Expected live cell at x=${x},y=${y}`);
         });
 
         expectDeadLocations.forEach(({ x, y }) => {
-          const data = sim.state.data.get({ x, y });
-          expect(data.length).eq(0, `Unexpected living cell at x=${x},y=${y}`);
+          const data = sim.state.data.get({ x, y, state: 0 });
+          expect(data.length).eq(0, `Expected dead cell at x=${x},y=${y}`);
         });
       });
     });
