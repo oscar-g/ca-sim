df97889022647005b90c4fd26cc9a021b71d5e69
Oscar Gonzalez <oscarg610@gmail.com>
2018-09-02 16:45:58 -0400

    more State tests

--- test/simulation/State.spec.ts
+++ test/simulation/State.spec.ts
@@ -3,6 +3,98 @@ import { expect } from 'chai';
 import State from '../../src/simulation/State';
 
 describe.only('State', () => {
+  describe('setData()', () => {
+    it('updates live cells', () => {
+      const state = new State([
+        [0, 1],
+        [1, 0],
+      ]);
+
+      // turn off
+      state.setData({
+        x: 1,
+        y: 0,
+        state: 0,
+      });
+
+      const testOpt = state.getData({ x: 1, y: 0 });
+      expect(testOpt.nonEmpty).true;
+      expect(testOpt.get.state).eq(0);
+    });
+
+    it('sets data on dead cells', () => {
+      const state = new State([
+        [0, 1],
+        [1, 0],
+      ]);
+
+      // turn on a cell
+      state.setData({
+        x: 0,
+        y: 0,
+        state: 1,
+      });
+
+      const testOpt = state.getData({ x: 0, y: 0 });
+      expect(testOpt.nonEmpty).true;
+      expect(testOpt.get.state).eq(1);
+    });
+
+    it('implements toriodal data array', () => {
+      const state = new State([
+        [0, 1],
+        [1, 0],
+      ]);
+
+      // turn on
+      state.setData({
+        x: 2,
+        y: 2,
+        state: 1,
+      });
+
+      let testOpt = state.getData({ x: 0, y: 0 });
+      expect(testOpt.nonEmpty).true;
+      expect(testOpt.get.state).eq(1);
+
+      // turn off
+      state.setData({
+        x: 2,
+        y: 2,
+        state: 0,
+      });
+
+      testOpt = state.getData({ x: 0, y: 0 });
+      expect(testOpt.nonEmpty).true;
+      expect(testOpt.get.state).eq(0);
+    });
+  });
+
+  describe('delData()', () => {
+    it('removes data', () => {
+      const state = new State([
+        [0, 1],
+        [1, 0],
+      ]);
+
+      state.delData({ x: 1, y: 0 });
+      expect(state.getData({ x: 1, y: 0 }).isEmpty).true;
+    });
+
+    it('implements toriodal data array', () => {
+      const state = new State([
+        [0, 1],
+        [1, 0],
+      ]);
+
+      state.delData({ x: 3, y: 0 });
+      expect(state.getData({ x: 1, y: 0 }).isEmpty).true;
+
+      state.delData({ x: 0, y: 3 });
+      expect(state.getData({ x: 0, y: 1 }).isEmpty).true;
+    });
+  });
+
   describe('getData()', () => {
     it('returns the correct data', () => {
       const state = new State([
@@ -32,7 +124,7 @@ describe.only('State', () => {
       });
     });
 
-    it('considers data array to be a toriod', () => {
+    it('implements toriodal data array', () => {
       const state = new State([
         [0, 1],
         [1, 0],
